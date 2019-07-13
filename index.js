d3ce272e768ef2b1ee46cd963e040c9fb0127da9
Sharon Williams <swilliams@xandium.io>
2019-07-13 19:04:15 -0400

    Formatting fix and package naming change
    dependency fix
    
    dependency fix

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const amqp = require('amqp')
-const config = require('./config/config.json')
-const CloudStorm = require('cloudstorm').Client
-const app = express()
-const bot = new CloudStorm(config.token, config.botConfig)
-const shardRouter = require('./routes/shardStatusRoutes')
-const gatewayRouter = require('./routes/gatewayRoutes')
-let StatsD
-let statsClient
+const express = require("express");
+const bodyParser = require("body-parser");
+const amqp = require("amqp");
+const config = require("./config/config.json");
+const CloudStorm = require("@xandium/cloudstorm").Client;
+const app = express();
+const bot = new CloudStorm(config.token, config.botConfig);
+const shardRouter = require("./routes/shardStatusRoutes");
+const gatewayRouter = require("./routes/gatewayRoutes");
+let StatsD;
+let statsClient;
 try {
-  StatsD = require('hot-shots')
-} catch (e) {
-
-}
+  StatsD = require("hot-shots");
+} catch (e) {}
 if (StatsD && config.statsD && config.statsD.enabled) {
-  statsClient = new StatsD(config.statsD)
+  statsClient = new StatsD(config.statsD);
 }
-const version = require('./package.json').version
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+const version = require("./package.json").version;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use((req, res, next) => {
-  req.bot = bot
-  next()
-})
-app.use('/shards', shardRouter)
-app.use('/gateway', gatewayRouter)
-app.all('/', (req, res) => {
-  res.json({ version: version, gatewayVersion: bot.version })
-})
-app.listen(config.port, config.host)
-const connection = amqp.createConnection(config.amqpUrl)
-connection.on('error', (e) => {
-  console.error(e)
-})
-connection.on('ready', async () => {
-  console.log('AMQP Connection ready')
-  await bot.connect()
-  bot.on('event', (event) => {
+  req.bot = bot;
+  next();
+});
+app.use("/shards", shardRouter);
+app.use("/gateway", gatewayRouter);
+app.all("/", (req, res) => {
+  res.json({ version: version, gatewayVersion: bot.version });
+});
+app.listen(config.port, config.host);
+const connection = amqp.createConnection(config.amqpUrl);
+connection.on("error", e => {
+  console.error(e);
+});
+connection.on("ready", async () => {
+  console.log("AMQP Connection ready");
+  await bot.connect();
+  bot.on("event", event => {
     if (statsClient) {
-      statsClient.increment(`discordevent`, 1, 1, [`shard:${event.shard_id}`, `event:${event.t}`], (err) => {
-        if (err) {
-          console.log(err)
-        }
-      })
-      if (event.t !== 'PRESENCE_UPDATE') {
-        statsClient.increment(`discordevent.np`, 1, 1, [`shard:${event.shard_id}`, `event:${event.t}`], (err) => {
+      statsClient.increment(
+        `discordevent`,
+        1,
+        1,
+        [`shard:${event.shard_id}`, `event:${event.t}`],
+        err => {
           if (err) {
-            console.log(err)
+            console.log(err);
           }
-        })
+        }
+      );
+      if (event.t !== "PRESENCE_UPDATE") {
+        statsClient.increment(
+          `discordevent.np`,
+          1,
+          1,
+          [`shard:${event.shard_id}`, `event:${event.t}`],
+          err => {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
       }
     }
-    connection.publish(config.amqpQueue, event)
+    connection.publish(config.amqpQueue, event);
     // Event was sent to amqp queue, now you can use it somewhere else
-  })
-})
-bot.on('ready', () => {
-  console.log(`Bot is ready with ${Object.keys(bot.shardManager.shards).length} shards`)
-})
+  });
+});
+bot.on("ready", () => {
+  console.log(
+    `Bot is ready with ${Object.keys(bot.shardManager.shards).length} shards`
+  );
+});
 // bot.on('event', (event) => {
 //     console.log(event);
 // });
 
-console.log(`Server listening on ${config.host}:${config.port}`)
+console.log(`Server listening on ${config.host}:${config.port}`);
--- routes/shardStatusRoutes.js
+++ routes/shardStatusRoutes.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
-router.get('/status', (req, res) => {
-  const shards = Object.keys(req.bot.shardManager.shards)
+const router = require("express").Router();
+router.get("/status", (req, res) => {
+  const shards = Object.keys(req.bot.shardManager.shards);
   for (let i = 0; i < shards.length; i++) {
     shards[i] = {
       id: req.bot.shardManager.shards[shards[i]].id,
@@ -8,19 +8,23 @@ router.get('/status', (req, res) => {
       ready: req.bot.shardManager.shards[shards[i]].ready,
       trace: req.bot.shardManager.shards[shards[i]].connector._trace,
       seq: req.bot.shardManager.shards[shards[i]].connector.seq
-    }
+    };
   }
-  return res.status(200).json({ shards, endpoint: req.bot.options.endpoint })
-})
-router.get('/queue', (req, res) => {
-  let queue = req.bot.shardManager.connectQueue
-  queue = queue.map(shard => ({ id: shard.id }))
-  return res.status(200).json({ queue, lastConnect: req.bot.shardManager.lastConnectionAttempt })
-})
-router.get('/:id', (req, res) => {
-  const shard = req.bot.shardManager.shards[req.params.id]
+  return res.status(200).json({ shards, endpoint: req.bot.options.endpoint });
+});
+router.get("/queue", (req, res) => {
+  let queue = req.bot.shardManager.connectQueue;
+  queue = queue.map(shard => ({ id: shard.id }));
+  return res
+    .status(200)
+    .json({ queue, lastConnect: req.bot.shardManager.lastConnectionAttempt });
+});
+router.get("/:id", (req, res) => {
+  const shard = req.bot.shardManager.shards[req.params.id];
   if (!shard) {
-    return res.status(404).json({ status: 404, message: `Shard ${req.params.id} does not exist` })
+    return res
+      .status(404)
+      .json({ status: 404, message: `Shard ${req.params.id} does not exist` });
   }
   const shardData = {
     id: shard.id,
@@ -28,8 +32,8 @@ router.get('/:id', (req, res) => {
     ready: shard.ready,
     trace: shard.connector._trace,
     seq: shard.connector.seq
-  }
-  return res.status(200).json(shardData)
-})
+  };
+  return res.status(200).json(shardData);
+});
 
-module.exports = router
+module.exports = router;
